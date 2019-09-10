c764da57aabab16e77e1c659407f63cccd10b8df
Fahad <fahadbinmnr@gmail.com>
2019-09-10 21:32:04 +0600

    sorting the list

---
+++ List/list_sort.py
@@ -0,0 +1,15 @@
+
+
+university_id = [1222,12234,1223,1225,1223,12226,12245,12232,12231,12275,12237]
+
+for student in university_id:
+
+    print(student)
+
+university_id.sort()
+print('\n')
+print('after sorting the list')
+print('------------------------')
+print('\n')
+for student in university_id:
+    print(student)
